80add45123b2c742811a03d22c94d4f4c3f84dc5
lexxai <lexxai@users.noreply.github.com>
2023-06-23 03:27:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,30 +7,44 @@ from  random import Random
 import calendar
 
 
-def parse_data(bd: date | datetime | str) -> date:
+def parse_data(bd: date | datetime | str) -> date | None:
     user_bd = None
-    if isinstance(bd, datetime) :
-        user_bd = bd.date()
-    elif isinstance(bd, date):
-         user_bd = bd
-    elif isinstance(bd, str):
-        if len(bd.split('-')) == 2:
-            user_bd = datetime.strptime(bd, "%Y-%m-%d").date()
-        elif len(bd.split('.')) == 2:
-            user_bd = datetime.strptime(bd, "%d.%m.%Y").date()
-        else:
-            user_bd = date.fromisoformat(bd)
-    #     print("format of date undefined, skip")
+    try:
+        if isinstance(bd, datetime) :
+            user_bd = bd.date()
+        elif isinstance(bd, date):
+            user_bd = bd
+        elif isinstance(bd, str):
+            if len(bd.split('-')) == 3:
+                user_bd = datetime.strptime(bd, "%Y-%m-%d").date()
+            elif len(bd.split('.')) == 3:
+                user_bd = datetime.strptime(bd, "%d.%m.%Y").date()
+            else:
+                user_bd = date.fromisoformat(bd)
+    except  ValueError as e:
+        print("format of date undefined", bd, e)
+        ...
 
     return user_bd
+
+def print_users(weekdays: dict) -> None:
+    for weekday, users in sorted(weekdays.items()):
+        if isinstance(weekday, date) or isinstance(weekday, datetime):
+            weekday = weekday.strftime("%A, %x")
+        userlist = [user['name'] for user in users]
+        print(f"{weekday} : {userlist}")
+        
+
 
 
 def get_birthdays_per_week(users: list | tuple) -> list:
 
     result = defaultdict(list)
     current_datetime = datetime.now().date()
-    start_weeks_interval = timedelta(days=5-current_datetime.weekday())
+    curr_week = current_datetime.weekday()
+    start_weeks_interval = timedelta(days=5-curr_week)
     end_weeks_interval = timedelta(days=6)
+    next_monday = current_datetime + timedelta(days=7-curr_week)
     start_week = current_datetime + start_weeks_interval
     end_week = start_week + end_weeks_interval
 
@@ -38,18 +52,20 @@ def get_birthdays_per_week(users: list | tuple) -> list:
         user_bd = parse_data(user["birthday"])
         if not user_bd :
             print(f"format of date undefined, skip, user {user['name']}")
+            continue
         user_bd_this_year = user_bd.replace(year=current_datetime.year)
         if  start_week <= user_bd_this_year <= end_week:
             user_week = user_bd_this_year.weekday()
             user_bd_day  = calendar.day_name[user_week]
-            if user_week > 5:
+            if user_week >= 5:
                 user['weekend'] = user_bd_day
-                user_week = 0
-            day = user_week
+                day = next_monday
+            else:
+                day = user_bd_this_year
             result[day].append(user)
-    result_map = {calendar.day_name[k]: v for k, v in sorted(result.items())}
+    #result_map = {calendar.day_name[k]: v for k, v in sorted(result.items())}
 
-    return result_map
+    return result
 
 
 
@@ -63,16 +79,17 @@ if __name__ == "__main__":
 
 
     f = Faker(lang)
+    #current_date = datetime.now().date()
     current_date = datetime.now().date()
     
 #                "birthday": (current_date + timedelta(days=Random().randrange(1,30))).strftime("%Y-%m-%d")
     users = ( { "name": f.name() ,  
-                "birthday": (current_date + timedelta(days=Random().randrange(1,30)))
+                "birthday": (current_date + timedelta(days=Random().randrange(1, 30))).strftime("%d.%m.%Y")
             } for i in range(50) )
 
     #print(users)
     birthday_result = get_birthdays_per_week(users)
-    print(birthday_result)
+    print_users(birthday_result)
 
 
 """
